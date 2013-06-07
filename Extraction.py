7359c1280ff411b506586b00c439fc03da0ea01c
bingham <wbx123456@gmail.com>
2013-06-07 16:24:55 +0800

    重构

--- Extraction.py
+++ Extraction.py
@@ -1,85 +1,135 @@
-import nltk
+# import nltk
 import re
 from gensim import corpora, models, similarities
-#from nltk.corpus import stopwords
-stop = open('data/english').read()
-stopwords = nltk.word_tokenize(stop)
-f = open('data/AR/raw-data.txt', 'rU')
-vocab_list = []
-# output_file = open('data/corpus_lsi.txt', 'w')
-open_file = open('data/corpus_lsi.txt', 'r')
-# for line in f:
-# 	raw = line.strip()
-# 	#raw = f.read()
-# 	tokens = re.findall('[a-zA-Z]+', raw)
-# 	#wnl = nltk.WordNetLemmatizer()
-# 	#raw = nltk.clean_html(html)
-# 	#tokens = nltk.word_tokenize(raw)
-# 	#porter = nltk.PorterStemmer()
-# 	#stem = [porter.stem(t) for t in tokens]
-# 	#stem = [wnl.lemmatize(t) for t in tokens]
-# 	words = [w.lower() for w in tokens]
-# 	newwords = [w for w in words if w not in stopwords]
-# 	vocab = sorted(set(newwords))
-# 	#word_freq = FreqDist(words)
-# 	#text = nltk.Text(tokens)
-# 	#print word_freq
-# 	vocab_list.append(vocab)
-	#print vocab_list
-# print vocab_list
-# dic = corpora.Dictionary(vocab_list)
-# dic.save('data/dic.dict')
-dic = corpora.Dictionary().load('data/dic.dict')
-# corpus = [dic.doc2bow(v) for v in vocab_list]
-corpus = corpora.MmCorpus('data/corpus.mm')
-# print list(corpus)
-# corpora.MmCorpus.serialize('data/corpus.mm', corpus) # store to disk, for later use
-# tfidf = models.TfidfModel(corpus)
-# tfidf.save('data/tfidf.tfidf_model')
-tfidf = models.TfidfModel().load('data/tfidf.tfidf_model')
 
-# print tfidf.dfs
-corpus_tfidf = tfidf[corpus]
-# print list(corpus_tfidf)
-# lsi = models.LsiModel(corpus_tfidf, id2word = dic, num_topics = 400)
-lsi = models.LsiModel.load('data/lsi.model')
-# lsi.save('data/lsi.model')
-# lsi.load('data/lsi.npy')
-# print lsi.print_topics(50)
-corpus_lsi = lsi[corpus_tfidf]
-index = similarities.MatrixSimilarity.load('data/index.lsi_index')
-query_list = list()
-for line in open_file.readlines():
-	# each_list = list()
-	# for elm in eval(line):
-		# each_list.append(elm)
-	query_list.append(line)
-# print type(query_list[0])
-open_file.close()
-query_lsi = list()
-for elem in eval(query_list[0]):
-	query_lsi.append(elem)
-# print query_lsi
-sims = index[query_lsi]
-# for vec in corpus_lsi:
-	# output_file.write(str(vec) + '\n')
-# output_file.close()
-# print query_lsi
-# index = similarities.MatrixSimilarity(lsi[corpus])
-# index.save('data/index.lsi_index')
-# vec = list(corpus_tfidf)[0]
-# print vec
-# print tfidf[vec]
+def vocab_list(raw_data_file = 'data/AR/raw-data.txt', stopwords_file = 'data/english'):
+	vocab_list = []
+	stop = open(stopwords_file, 'r').read()
+	stopwords = nltk.word_tokenize(stop)
+	raw_data = open(raw_data_file, 'rU')
+	for line in raw_data:
+		raw = line.strip()
+		tokens = re.findall('[a-zA-Z]+', raw)
+		words = [w.lower() for w in tokens]
+		clear_words = [w for w in words if w not in stopwords]
+		vocab = sorted(set(clear_words))
+		vocab_list.append(vocab)
+	return vocab_list
+def create_dictionary(vocab_list, dic_file_dir = 'data/dic.dict'):
+	dic = corpora.Dictionary(vocab_list)
+	dic.save(dic_file_dir)
+	return dic
+def create_corpus(vocab_list, corpus_file_dir = 'data/corpus.mm'):
+	corpus = [dic.doc2bow(v) for v in vocab_list]
+	corpora.MmCorpus.serialize(corpus_file_dir, corpus)
+	return corpus
+def create_tfidf_model(corpus, tfidf_file_dir = 'data/tfidf.tfidf_model'):
+	tfidf = models.TfidfModel(corpus)
+	tfidf.save(tfidf_file_dir)
+	return tfidf
+def create_lsi_model(dic, corpus, tfidf, lsi_file_dir = 'data/lsi.model', topic_num = 400):
+	corpus_tfidf = tfidf[corpus]
+	lsi = models.LsiModel(corpus_tfidf, id2word = dic, num_topics = topic_num)
+	lsi.save(lsi_file_dir)
+	return lsi
+def create_corpus_lsi(lsi, corpus_tfidf, corpus_lsi_file_dir = 'data/corpus_lsi.txt'):
+	corpus_lsi = lsi[corpus_tfidf]
+	output_file = open(corpus_lsi_file_dir, 'w')
+	for vec in corpus_lsi:
+		output_file.write(str(vec) + '\n')
+	output_file.close()
+	return corpus_lsi
+def create_index(lsi, corpus, index_file_dir = 'data/index.lsi_index'):
+	index = similarities.MatrixSimilarity(lsi[corpus])
+	index.save(index_file_dir)
+	return index
+if __name__ == '__main__':
+	vocab_list = vocab_list()
+	dic = create_dictionary(vocab_list)
+	corpus = create_corpus(vocab_list)
+	tfidf = create_tfidf_model(corpus)
+	lsi = create_lsi_model(dic, corpus, tfidf)
+	corpus_lsi = create_corpus_lsi(lsi, tfidf[corpus])
+	index = create_index(lsi, corpus)
 
-# index.save('data/index.npy')
-# index = similarities.SparseMatrixSimilarity().load('data/index.npy')
-# #query = "that are composed of elaborate hierarchies of protocols and layers of feedback regulation"
-# #query_bow = dic.doc2bow(query.lower().split())
-# #query_lsi = corpus_lsi[1]
-# sims = index[vec]
+
+# stop = open('data/english').read()
+# stopwords = nltk.word_tokenize(stop)
+# f = open('data/AR/raw-data.txt', 'rU')
+# vocab_list = []
+# # output_file = open('data/corpus_lsi.txt', 'w')
+# open_file = open('data/corpus_lsi.txt', 'r')
+# # for line in f:
+# # 	raw = line.strip()
+# # 	#raw = f.read()
+# # 	tokens = re.findall('[a-zA-Z]+', raw)
+# # 	#wnl = nltk.WordNetLemmatizer()
+# # 	#raw = nltk.clean_html(html)
+# # 	#tokens = nltk.word_tokenize(raw)
+# # 	#porter = nltk.PorterStemmer()
+# # 	#stem = [porter.stem(t) for t in tokens]
+# # 	#stem = [wnl.lemmatize(t) for t in tokens]
+# # 	words = [w.lower() for w in tokens]
+# # 	newwords = [w for w in words if w not in stopwords]
+# # 	vocab = sorted(set(newwords))
+# # 	#word_freq = FreqDist(words)
+# # 	#text = nltk.Text(tokens)
+# # 	#print word_freq
+# # 	vocab_list.append(vocab)
+# 	#print vocab_list
+# # print vocab_list
+# # dic = corpora.Dictionary(vocab_list)
+# # dic.save('data/dic.dict')
+# dic = corpora.Dictionary().load('data/dic.dict')
+# # corpus = [dic.doc2bow(v) for v in vocab_list]
+# corpus = corpora.MmCorpus('data/corpus.mm')
+# # print list(corpus)
+# # corpora.MmCorpus.serialize('data/corpus.mm', corpus) # store to disk, for later use
+# # tfidf = models.TfidfModel(corpus)
+# # tfidf.save('data/tfidf.tfidf_model')
+# tfidf = models.TfidfModel().load('data/tfidf.tfidf_model')
+
+# # print tfidf.dfs
+# corpus_tfidf = tfidf[corpus]
+# # print list(corpus_tfidf)
+# # lsi = models.LsiModel(corpus_tfidf, id2word = dic, num_topics = 400)
+# lsi = models.LsiModel.load('data/lsi.model')
+# # lsi.save('data/lsi.model')
+# # print lsi.print_topics(50)
+# corpus_lsi = lsi[corpus_tfidf]
+# index = similarities.MatrixSimilarity.load('data/index.lsi_index')
+# query_list = list()
+# for line in open_file.readlines():
+# 	# each_list = list()
+# 	# for elm in eval(line):
+# 		# each_list.append(elm)
+# 	query_list.append(line)
+# # print type(query_list[0])
+# open_file.close()
+# query_lsi = list()
+# for elem in eval(query_list[0]):
+# 	query_lsi.append(elem)
+# # print query_lsi
+# sims = index[query_lsi]
+# # for vec in corpus_lsi:
+# 	# output_file.write(str(vec) + '\n')
+# # output_file.close()
+# # print query_lsi
+# # index = similarities.MatrixSimilarity(lsi[corpus])
+# # index.save('data/index.lsi_index')
+# # vec = list(corpus_tfidf)[0]
+# # print vec
+# # print tfidf[vec]
+
+# # index.save('data/index.npy')
+# # index = similarities.SparseMatrixSimilarity().load('data/index.npy')
+# # #query = "that are composed of elaborate hierarchies of protocols and layers of feedback regulation"
+# # #query_bow = dic.doc2bow(query.lower().split())
+# # #query_lsi = corpus_lsi[1]
+# # sims = index[vec]
 # print list(enumerate(sims))
-sort_sims = sorted(enumerate(sims), key=lambda item: -item[1])
-print sort_sims[:10]
+# sort_sims = sorted(enumerate(sims), key=lambda item: -item[1])
+# print sort_sims[:10]
 # # # for doc in corpus_lsi:
 # # # 	print doc
 # # # 	#output_file.write(doc + "\n")
--- Learning.py
+++ Learning.py
@@ -1,81 +1,143 @@
 from gensim import corpora, models, similarities
-f = open('data/AR/user-info-train.txt', 'r')
-corpus_lsi = open('data/corpus_lsi.txt', 'r')
-# output = open('data/user_profile.txt', 'w')
-user_profile_list = list()
-user_like_list = list()
-i = 1
-for line in f.readlines():
-	line.strip()
-	# print line
-	if(i == int(line.split(',')[0])):
-		# print 'hello'
-		user_like_list.append(int(line.split(',')[1]))
-	else:
-		i += 1
-		user_profile_list.append(user_like_list)
-		user_like_list = list()
-		user_like_list.append(int(line.split(',')[1]))
-	# user_like_list.append(int(line.split(',')[1]))
-	# print (int(line.split(',')[0]))
-# print len(user_profile_list)
-user_profile_list.append(user_like_list)
-# print type(user_profile_list[0][0])
-f.close()
 
-query_list = list()
-for line in corpus_lsi.readlines():
-	query_list.append(line)
-# print type(query_list[0])
-corpus_lsi.close()
-
-def query_to_lsi(item):
-	query_lsi = list()
-	for elem in eval(query_list[item - 1]):
+def str_to_lsi(item, corpus_lsi_list):
+	corpus_lsi = list()
+	for elem in eval(corpus_lsi_list[item - 1]):
 		query_lsi.append(elem)
-	return query_lsi
+	return corpus_lsi
+
 def tuple_sum(tuple_1, tuple_2):
 	return (tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1])
+
 def tuple_list_sum(list_1, list_2):
 	sumed_tuple_list = list()
 	for (i, j) in zip(list_1, list_2):
 		sumed_tuple_list.append(tuple_sum(i, j))
 	return sumed_tuple_list
+
 def tuple_div(tuples, div):
 	return (tuples[0]/div, tuples[1]/div)
+
 def tuple_list_average(tuple_list, div):
 	avg_tuple_list = list()
 	for tuples in tuple_list:
 		avg_tuple_list.append(tuple_div(tuples, div))
 	return avg_tuple_list
-# print query_to_lsi(1101)
-avg_profile_lsi = list()
-for user_like_list in user_profile_list:
-	# user_like_lsi = list()
-	sumed_tuple_list = list()
-	for item in user_like_list:
-		# print len(user_like_list)
-		# print item
-		# print query_list[item-1]
-		if(len(sumed_tuple_list) == 0):
-			sumed_tuple_list = query_to_lsi(item)
-			# print sumed_tuple_list
+
+def get_corpus_lsi_list(corpus_lsi_file_dir = 'data/corpus_lsi.txt'):
+	corpus_lsi = open(corpus_lsi_file_dir, 'r')
+	corpus_lsi_list = list()
+	for line in corpus_lsi.readlines():
+		corpus_lsi_list.append(line)
+	return corpus_lsi_list
+
+def get_user_profile_list(train_file_dir = 'data/AR/user-info-train.txt'):
+	train_file = open(train_file_dir, 'r')
+	user_profile_list = list()
+	user_like_list = list()
+	i = 1
+	for line in train_file.readlines():
+		line.strip()
+		if(i == int(line.split(',')[0])):
+			user_like_list.append(int(line.split(',')[1]))
 		else:
-			sumed_tuple_list = tuple_list_sum(query_to_lsi(item), sumed_tuple_list)
-		# user_like_lsi.append(query_to_lsi(item))
-		# sumed_tuple_list = tuple_list_sum(query_to_lsi(item), sumed_tuple_list)
-	# sumed_tuple_list = user_like_lsi[0]
-# 	for i in user_like_lsi:
-# 		if i[0][1] != sumed_tuple_list[0][1]:
-# 			sumed_tuple_list = tuple_list_sum(i, sumed_tuple_list)
-# 	# print len(user_like_lsi)
-	avg_tuple_list = tuple_list_average(sumed_tuple_list, len(user_like_list))
-	# output.write(str(avg_tuple_list) + '\n')
-	avg_profile_lsi.append(avg_tuple_list)
-# 	# print len(avg_profile_lsi)
-# 	# break
-# # print sumed_tuple_list
-print len(avg_profile_lsi)
-output.close()
+			i += 1
+			user_profile_list.append(user_like_list)
+			user_like_list = list()
+			user_like_list.append(int(line.split(',')[1]))
+	user_profile_list.append(user_like_list)
+	train_file.close()
+	return user_profile_list
+
+def get_avg_profile_list(user_profile_list, avg_profile_file_dir = 'data/user_profile.txt'):
+	avg_profile_file = open(avg_profile_file_dir, 'w')
+	avg_profile_list = list()
+	corpus_lsi_list = get_corpus_lsi_list()
+	for user_like_list in user_profile_list:
+		sumed_tuple_list = list()
+		for item in user_like_list:
+			if(len(sumed_tuple_list) == 0):
+				sumed_tuple_list = str_to_lsi(item, corpus_lsi_list)
+			else:
+				sumed_tuple_list = tuple_list_sum(str_to_lsi(item, corpus_lsi_list), sumed_tuple_list)
+		avg_tuple_list = tuple_list_average(sumed_tuple_list, len(user_like_list))
+		avg_profile_file.write(str(avg_tuple_list) + '\n')
+		avg_profile_list.append(avg_tuple_list)
+	avg_profile_file.close()
+	return avg_profile_list
+
+def get_all_similarity_with_avg_profile(avg_profile_list, index_file_dir = 'data/index.lsi_index', simi_file_dir = 'data/all_similarity.txt'):
+	simi_file = open(simi_file_dir, 'w')
+	index = similarities.MatrixSimilarity.load(index_file_dir)
+	for profile in avg_profile_list:
+		sims = index[profile]
+		sort_sims = sorted(enumerate(sims), key=lambda item: -item[1])
+		simi_file.write(str(sort_sims) + '\n')
+	simi_file.close()
+
+if __name__ == '__main__':
+	user_profile_list = get_user_profile_list()
+	avg_profile_list = get_avg_profile_list(user_profile_list)
+	get_all_similarity_with_avg_profile(avg_profile_list)
+
+
+# f = open('data/AR/user-info-train.txt', 'r')
+# corpus_lsi = open('data/corpus_lsi.txt', 'r')
+# # output = open('data/user_profile.txt', 'w')
+# user_profile_list = list()
+# user_like_list = list()
+# i = 1
+# for line in f.readlines():
+# 	line.strip()
+# 	# print line
+# 	if(i == int(line.split(',')[0])):
+# 		# print 'hello'
+# 		user_like_list.append(int(line.split(',')[1]))
+# 	else:
+# 		i += 1
+# 		user_profile_list.append(user_like_list)
+# 		user_like_list = list()
+# 		user_like_list.append(int(line.split(',')[1]))
+# 	# user_like_list.append(int(line.split(',')[1]))
+# 	# print (int(line.split(',')[0]))
+# # print len(user_profile_list)
+# user_profile_list.append(user_like_list)
+# # print type(user_profile_list[0][0])
+# f.close()
+
+# query_list = list()
+# for line in corpus_lsi.readlines():
+# 	query_list.append(line)
+# # print type(query_list[0])
+# corpus_lsi.close()
+# # print query_to_lsi(1101)
+# avg_profile_lsi = list()
+# for user_like_list in user_profile_list:
+# 	# user_like_lsi = list()
+# 	sumed_tuple_list = list()
+# 	for item in user_like_list:
+# 		# print len(user_like_list)
+# 		# print item
+# 		# print query_list[item-1]
+# 		if(len(sumed_tuple_list) == 0):
+# 			sumed_tuple_list = query_to_lsi(item)
+# 			# print sumed_tuple_list
+# 		else:
+# 			sumed_tuple_list = tuple_list_sum(query_to_lsi(item), sumed_tuple_list)
+# 		# user_like_lsi.append(query_to_lsi(item))
+# 		# sumed_tuple_list = tuple_list_sum(query_to_lsi(item), sumed_tuple_list)
+# 	# sumed_tuple_list = user_like_lsi[0]
+# # 	for i in user_like_lsi:
+# # 		if i[0][1] != sumed_tuple_list[0][1]:
+# # 			sumed_tuple_list = tuple_list_sum(i, sumed_tuple_list)
+# # 	# print len(user_like_lsi)
+# 	avg_tuple_list = tuple_list_average(sumed_tuple_list, len(user_like_list))
+# 	# output.write(str(avg_tuple_list) + '\n')
+# 	avg_profile_lsi.append(avg_tuple_list)
+# # 	# print len(avg_profile_lsi)
+# # 	# break
+# # # print sumed_tuple_list
+# print len(avg_profile_lsi)
+# output.close()
 
 
